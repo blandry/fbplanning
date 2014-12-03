686bd42dcd01d9a4b99bdf007e946b8d7246c983
Benoit Landry <landry@mit.edu>
2014-12-03 14:18:52 -0500

    some traj opt modifications

--- glider/runFullPlanningGlider.m
+++ glider/runFullPlanningGlider.m
@@ -1,20 +1,23 @@
-
 megaclear();
 
 options.floating = true;
 p = RigidBodyManipulator('GliderBalanced.urdf',options);
 
-options.N = 21;
+options.N = 41;
 options.minimum_duration = .1;
 options.maximum_duration = 4;
-options.x0 = [-3.3 0 .4 0 0 0 0  7 0 0 0 0 0 0]';
-options.xf = [0 0 0 0 -pi/4 0 0  0 0 -1 0 0 0 0]';
+
+options.x0lb = [-10 0 .1 0 0 0 0 0 0 0 0 0 0 0]';
+options.x0ub = [-5 0 3 0 0 0 0 10 0 0 0 0 0 0]';
+
+options.xlb = [-15 0 -10 0 -pi 0 -pi/2 -Inf -Inf -Inf -Inf -Inf -Inf -Inf]';
+options.xub = [15 0 10 0 pi 0 pi/2 Inf Inf Inf Inf Inf Inf Inf]';
+
+options.xflb = [0 0 0 0 -pi 0 -pi/2 -Inf 0 -Inf 0 -Inf 0 -Inf]';
+options.xfub = [0 0 0 0 0 0 pi/2 Inf 0 Inf 0 Inf 0 Inf]';
 
 % 1- SOLVE FOR A TRAJECTORY USING A BRICK
-[bricktraj, forcetraj] = runFlyingBrickPlanning(p,options);
-% data = load('brick.mat');
-% bricktraj = data.bricktraj;
-% forcetraj = data.forcetraj;
+% [bricktraj, forcetraj] = runFlyingBrickPlanning(p,options);
 
 % sanity check of the brick plan
 % breaks = bricktraj.getBreaks();
@@ -28,25 +31,25 @@ options.xf = [0 0 0 0 -pi/4 0 0  0 0 -1 0 0 0 0]';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-% 2- SOLVE THE REVERSE KINEMATICS PROBLEM
-t = bricktraj.getBreaks();
-brickpos = bricktraj.eval(t);
-forces = forcetraj.eval(t);
-w = warning('off','Drake:TaylorVar:DoubleConversion');
-xx = reverseKinBrick(p,brickpos,forces);
-warning(w);
-xtraj0 = PPTrajectory(spline(t,xx));
-
-% sanity check of the reverse kin for the forces
+% % 2- SOLVE THE REVERSE KINEMATICS PROBLEM
+% t = bricktraj.getBreaks();
+% brickpos = bricktraj.eval(t);
+% forces = forcetraj.eval(t);
+% w = warning('off','Drake:TaylorVar:DoubleConversion');
+% xx = reverseKinBrick(p,brickpos,forces);
+% warning(w);
+% xtraj0 = PPTrajectory(spline(t,xx));
+% 
+% % sanity check of the reverse kin for the forces
 % xtraj0 = setOutputFrame(xtraj0,getStateFrame(p));
-% v = p.constructVisualizer();
+% % v = p.constructVisualizer();
 % v.playback(xtraj0,struct('slider',true,'visualized_system',p));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 % 3- SOLVE FULL TRAJECTORY USING THE PREVIOUS RESULTS AS SEED
-options.xtraj0 = xtraj0;
+% options.xtraj0 = xtraj0;
 [xtraj, utraj] = runTrajOpt(p,options);
 
 % display the final trajectory
@@ -54,3 +57,4 @@ v = p.constructVisualizer();
 v.playback(xtraj,struct('slider',true));
 
 % simulates the final trajectory
+% TODO
--- glider/runTrajOpt.m
+++ glider/runTrajOpt.m
@@ -3,36 +3,59 @@ function [xtraj,utraj] = runTrajOpt(p,options)
 N = options.N;
 minimum_duration = options.minimum_duration;
 maximum_duration = options.maximum_duration;
-
 prog = DircolTrajectoryOptimization(p,N,[minimum_duration maximum_duration]);
+prog = prog.addStateConstraint(BoundingBoxConstraint(options.x0lb,options.x0ub),1);
+prog = prog.addStateConstraint(BoundingBoxConstraint(options.xlb,options.xub),1:N);
+prog = prog.addStateConstraint(BoundingBoxConstraint(options.xflb,options.xfub),N);
 
-prog = prog.addStateConstraint(ConstantConstraint(options.x0),1);
-prog = prog.addStateConstraint(ConstantConstraint(options.xf),N);
+%prog = prog.addRunningCost(@cost);
+%prog = prog.addFinalCost(@finalCost);
+prog = prog.addTrajectoryDisplayFunction(@plotDircolTraj);
 
-prog = prog.addRunningCost(@cost);
-prog = prog.addFinalCost(@finalCost);
-
-tf0 = options.xtraj0.tspan(2);
-traj_init.x = options.xtraj0;
-traj_init.u = ConstantTrajectory(zeros(getNumInputs(p),1));
+if (isfield(options,'xtraj0')&&isfield(options,'utraj0'))
+  tf0 = options.xtraj0.tspan(2);
+  traj_init0.x = options.xtraj0;
+  traj_init0.u = options.utraj0;
+else
+  tf0 = .5*(options.minimum_duration+options.maximum_duration);
+  traj_init0.x = PPTrajectory(foh([0,tf0],[options.x0lb,options.xflb]));
+  traj_init0.u = ConstantTrajectory(zeros(getNumInputs(p),1));
+  % traj_init0 = struct();
+end
 
 display('Running full body traj opt...');
-info=0;
-while (info~=1)
+for i=1:1
   tic
-  [xtraj,utraj,~,~,info] = prog.solveTraj(tf0,traj_init);
+  [xtraj,utraj,z,F,info,infeasible_constraint_name] = prog.solveTraj(tf0,traj_init0);
   toc
+  if info==1, break; end
 end
+if info~=1, error('Failed to find a trajectory'); end
 
 end
 
 function [g,dg] = cost(dt,x,u)
   R = eye(numel(u));
   g = u'*R*u;
-  dg = [zeros(1,1+numel(x)),2*u'*R];
+  if (nargout>1)
+    dg = [0,zeros(1,length(x)),2*u'*R];
+  end
 end
 
 function [h,dh] = finalCost(t,x)
-  h = 100*t;
-  dh = [100,zeros(1,size(x,1))];
+  h = t;
+  if (nargout>1)
+    dh = [1,zeros(1,numel(x))];
+  end
+end
+
+function plotDircolTraj(t,x,u)
+  figure(1)
+  clf;
+  hold on
+  plot(x(1,:),x(3,:),'.-');
+  hold off
+  axis([-15 15 -15 15]);
+  drawnow;
+  %delete(h);
 end
